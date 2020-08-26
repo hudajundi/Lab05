88ee3377a4cff3b84c7f9964b42b43454c30e8ee
hudajundi <huda.aljundi97@gmail.com>
2020-08-26 13:20:39 +0300

    add function

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var result;
 if ( apple == "yes") {
     result = ' good';
 } else {
+    ( apple == "no")
     result = ' Go eat';
 }
 document.write('<h3>'+result+'</h3>')
@@ -30,19 +31,45 @@ var picture;
 if (love =="yes") {
 document.write ("<img src='https://img.favpng.com/16/24/23/apple-red-delicious-perfume-fruit-png-favpng-cdrGhxzH27vcrPdAqN4x4THAA.jpg'> ")   
 } else {
-    decument.write  ("<img src='https://cdn.pixabay.com/photo/2019/10/03/01/44/apple-4522286_1280.png' >")  
+    (love == "no")
+    decument.write ("<img src='https://cdn.pixabay.com/photo/2019/10/03/01/44/apple-4522286_1280.png' >")  
 }
-document.write('<p>'+picture+'</p>')
 
 
-var gender = promp("Are you a boy ar a girl?")
+
+var gender = prompt("Are you a boy ar a girl?")
 var name ;
 
 if (gender = "boy" ) {
-    name = 'Hi, boy '
+    name = 'Hi, boy ';
     
 } else {
-    name = 'hi , girl'
+    name = 'hi , girl';
     
 } 
 document.write ('<h4>'+name+'</h4>')
+
+function WelomingFunction() {
+    alert ("Hello");
+    console.log()
+} 
+WelomingFunction();
+
+
+function Health(){
+    //input
+    var Weight = prompt('enter your weight');
+    var Health;
+
+    // processing
+    if (Weight > 100) {
+        document.write ("Obese");
+        
+    } else {
+        document.write("healthy");
+        
+    } 
+    //output
+    return Health
+}
+Health(); 
